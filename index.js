09536add875b8f6df30fe74c587f93603893cf35
udit_jaiswal <jaiswal.raunak.904@gmail.com>
2023-08-19 12:11:10 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+let timer = 60;
+let rn;
+let num;
+let scr = 0;
+
+function time() {
+
+    let a = setInterval(() => {
+        if (timer > 0) {
+            timer--;
+            document.querySelector("#timer").innerHTML = timer
+        }
+        else {
+            clearInterval(a);
+            document.querySelector("#bubblesinput").innerHTML = "";
+            result();
+
+        }
+    }, 1000)
+
+}
+function hitrn() {
+    rn = Math.floor(Math.random() * 10);
+    document.querySelector("#hits").innerHTML = rn
+}
+
+function bubbles() {
+    let buble = "";
+    for (let i = 1; i <= 152; i++) {
+        let rn = Math.floor(Math.random() * 10);
+        buble += `<div class="bubble">${rn}</div>`
+        document.querySelector("#bubblesinput").innerHTML = buble
+        document.querySelector("#bubblesinput").classList.add('hidden')
+    }
+}
+
+document.getElementById('bubblesinput').addEventListener('click', function (e) {
+    num = Number(e.target.innerHTML)
+    if (rn === num) {
+        scr += 10;
+        document.getElementById("score").innerHTML = `SCORE-${scr}`;
+        hitrn();
+        bubbles();
+    }
+})
+
+
+function result() {
+    document.querySelector("#bubblesinput").innerHTML = `<div  class='result'>
+        <div><h1>GAME-OVER</h1></div>
+        <div>YOUR SCORE - ${scr}</div>
+    </div>`
+
+
+}
+
+function start () {
+    document.getElementById("startbtn").addEventListener('click',function (){
+        bubbles();
+        time();
+        hitrn();
+    })
+}
+start();
+// bubbles();
